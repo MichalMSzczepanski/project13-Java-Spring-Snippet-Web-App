52eae1e83a3e4b2a8998687f0afe0ed9bd7b3fae
Michał Szczepański <michal.szczepanski@novaris.pl>
2021-09-04 23:40:46 +0200

    two DB created (local + hostinger); added account validation after 24 ( deleted from DB if not confirmed); added validation if last admin wants to delete last admin

---
+++ src/main/java/work/szczepanskimichal/project13snippetapp/utils/Languages.java
@@ -0,0 +1,38 @@
+package work.szczepanskimichal.project13snippetapp.utils;
+
+import lombok.RequiredArgsConstructor;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+// check below anotation on front
+//@RequiredArgsConstructor
+public enum Languages {
+
+    PYTHON("Python"),
+    JAVA("Java"),
+    C("C"),
+    JAVASCRIPT("JavaScript");
+
+    private String val;
+
+    // values - iteruje po enumach
+    private static List<Languages> languageList = Arrays.asList(values());
+
+    Languages(String val){
+        this.val = val;
+    }
+
+    public static List<String> getLanguages() {
+        return languageList.stream()
+                .map(l -> l.val)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String toString() {
+        return this.val;
+    }
+
+}
