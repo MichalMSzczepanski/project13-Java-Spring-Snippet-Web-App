1167c6b26fdfdeb335e49e527fc6f5a534be84b0
Michał Szczepański <michal.szczepanski@novaris.pl>
2021-12-02 15:50:15 +0100

    UserSnippetController and SnippetService cleaned up

--- src/main/java/work/szczepanskimichal/project13snippetapp/snippet/SnippetService.java
+++ src/main/java/work/szczepanskimichal/project13snippetapp/snippet/SnippetService.java
@@ -1,6 +1,7 @@
 package work.szczepanskimichal.project13snippetapp.snippet;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
@@ -11,17 +12,16 @@ import work.szczepanskimichal.project13snippetapp.user.User;
 import work.szczepanskimichal.project13snippetapp.user.UserService;
 import work.szczepanskimichal.project13snippetapp.utils.Languages;
 import work.szczepanskimichal.project13snippetapp.utils.UtilLists;
-
+import javax.servlet.http.HttpServletRequest;
 import javax.transaction.Transactional;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Transactional
 @Service
 @RequiredArgsConstructor
+@Log4j2
 public class SnippetService {
 
     private final SnippetRepository snippetRepository;
@@ -29,7 +29,10 @@ public class SnippetService {
     private final TagService tagService;
     private final UtilLists utilLists;
 
-    public String returnUserDashboard(CurrentUser currentUser, String folder, Long id, Model model){
+    public String getUserDashboard(CurrentUser currentUser,
+                                      String folder,
+                                      Long id,
+                                      Model model){
         model.addAttribute("currentUser", currentUser.getUser());
         List<String> folderList = findAllUserFolders(currentUser.getUser().getEmail());
         model.addAttribute("folderList", folderList);
@@ -44,6 +47,75 @@ public class SnippetService {
         return "user/dashboard";
     }
 
+    public String userAddNewSnippetGet(Model model) {
+        model.addAttribute("snippet", new Snippet());
+        model.addAttribute("newSnippet", true);
+        return "user/user-snippet-manage";
+    }
+
+    public String userAddNewSnippetPost(CurrentUser currentUser,
+                                        Snippet snippet,
+                                        HttpServletRequest request) {
+        setSnippetFolder(snippet, request.getParameter("inputtedFolder"));
+        setSnippetTagsAndUserTags(snippet,request.getParameter("inputtedTags") , currentUser.getUser());
+        saveSnippet(snippet, currentUser.getUser().getId());
+        log.info("User: " + currentUser.getUsername() + " added snippet: " + snippet.getId());
+        return "redirect:/user/snippet-details/" + snippet.getId();
+    }
+
+    public String editUserSnippetGet(CurrentUser currentUser,
+                                     Long id,
+                                     Model model) {
+        Snippet snippet = getUserSnippetByID(id);
+        if(snippet.getOwner().getId() == currentUser.getUser().getId()) {
+            model.addAttribute("snippet", snippet);
+            getProgrammingLanguagesAndUserFoldersAndUserTags(currentUser, model);
+//            return "user/user-snippet-edit";
+            model.addAttribute("newSnippet", false);
+            return "user/user-snippet-manage";
+        } else {
+            return "public/error";
+        }
+    }
+
+    public String editUserSnippetPost(CurrentUser currentUser,
+                                      Snippet snippet,
+                                      HttpServletRequest request,
+                                      Model model) {
+        setSnippetFolder(snippet, request.getParameter("inputtedFolder"));
+        setSnippetTagsAndUserTags(snippet,request.getParameter("inputtedTags") , currentUser.getUser());
+        saveSnippet(snippet, currentUser.getUser().getId());
+        log.info("User: " + currentUser.getUsername() + " edited snippet: " + snippet.getId());
+        return "redirect:/user/snippet-details/" + snippet.getId();
+    }
+
+    public String viewUserSnippetDetails(CurrentUser currentUser,
+                                         Long id,
+                                         Model model) {
+        Snippet snippet = getUserSnippetByID(id);
+        if(snippet.getOwner().getId() == currentUser.getUser().getId()) {
+            model.addAttribute("snippet", snippet);
+            return "user/user-snippet-details";
+        } else {
+            log.info("ERROR while User: " + currentUser.getUsername() + " tried to edit foreign snippet: " + snippet.getId());
+            return "public/error";
+        }
+    }
+
+    public String deleteUserSnippetById(Long id,
+                                        CurrentUser currentUser) {
+        if (snippetRepository.existsById(id)) {
+            if (getUserSnippetByID(id).getOwner().getId() == currentUser.getUser().getId()) {
+                // add confirmation in popup or another widok
+                snippetRepository.delete(getUserSnippetByID(id));
+                log.info("User: " + currentUser.getUsername() + " deleted snippet: " + snippetRepository.getById(id).getId());
+                return "redirect:/user/dashboard";
+            }
+        }
+        log.info("ERROR while User: " + currentUser.getUsername() + " tried to delete snippet: " + snippetRepository.getById(id).getId());
+        return "public/snippetErrorDeleted";
+    }
+
     public List<Snippet> findAllUserSnippets(String email) {
         return snippetRepository.findAllUserSnippets(email);
     }
@@ -56,7 +128,8 @@ public class SnippetService {
         return snippetRepository.findAllFoldersOfUser(email);
     }
 
-    public void saveSnippet(Snippet snippet, Long id) {
+    public void saveSnippet(Snippet snippet,
+                            Long id) {
         snippet.setOwner(userService.findByUserId(id));
         snippetRepository.save(snippet);
     }
@@ -65,24 +138,16 @@ public class SnippetService {
         return snippetRepository.getById(id);
     }
 
-    public String deleteUserSnippetById(Long id, CurrentUser currentUser) {
-        if (snippetRepository.existsById(id)) {
-            if (getUserSnippetByID(id).getOwner().getId() == currentUser.getUser().getId()) {
-                // add confirmation in popup or another widok
-                snippetRepository.delete(getUserSnippetByID(id));
-                return "redirect:/user/dashboard";
-            }
-        }
-        return "public/snippetErrorDeleted";
-    }
-
-    public void setSnippetFolder(Snippet snippet, String inputtedFolder) {
+    public void setSnippetFolder(Snippet snippet,
+                                 String inputtedFolder) {
         if (inputtedFolder != null && inputtedFolder != "") {
             snippet.setFolder(inputtedFolder);
         }
     }
 
-    public void setSnippetTagsAndUserTags(Snippet snippet, String inputtedTags, User currentUser) {
+    public void setSnippetTagsAndUserTags(Snippet snippet,
+                                          String inputtedTags,
+                                          User currentUser) {
         List<String> newTagStrings = new ArrayList<>();
         if (inputtedTags != null && inputtedTags != "") {
             newTagStrings = Arrays.asList(inputtedTags.trim().split("\\s*,+\\s*,*\\s*"));
@@ -101,7 +166,8 @@ public class SnippetService {
 
     }
 
-    public void getProgrammingLanguagesAndUserFoldersAndUserTags(@AuthenticationPrincipal CurrentUser currentUser, Model model) {
+    public void getProgrammingLanguagesAndUserFoldersAndUserTags(@AuthenticationPrincipal CurrentUser currentUser,
+                                                                 Model model) {
         List<String> programmingLanguages = Languages.getLanguages();
         List<String> folderList = (findAllUserFolders(currentUser.getUser().getEmail()).isEmpty() || findAllUserFolders(currentUser.getUser().getEmail()) == null)
                 ? utilLists.getDefaultFolder() : findAllUserFolders(currentUser.getUser().getEmail());
@@ -111,6 +177,6 @@ public class SnippetService {
         model.addAttribute("userTags", userTags);
     }
 
-    //TODO method for adding all values into snippet instead of controller method .... balagan
+    //TODO method for adding all values into snippet instead of controller method
 
 }
--- src/main/java/work/szczepanskimichal/project13snippetapp/snippet/UserSnippetController.java
+++ src/main/java/work/szczepanskimichal/project13snippetapp/snippet/UserSnippetController.java
@@ -6,100 +6,70 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-//import work.szczepanskimichal.project13snippetapp.user.CurrentUser;
-import work.szczepanskimichal.project13snippetapp.tag.Tag;
-import work.szczepanskimichal.project13snippetapp.tag.TagService;
 import work.szczepanskimichal.project13snippetapp.user.CurrentUser;
-import work.szczepanskimichal.project13snippetapp.user.User;
-import work.szczepanskimichal.project13snippetapp.user.UserService;
-import work.szczepanskimichal.project13snippetapp.utils.Languages;
-import work.szczepanskimichal.project13snippetapp.utils.UtilLists;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/user")
-//@Secured("ROLE_USER")
 public class UserSnippetController {
 
-    private final UtilLists utilLists;
     private final SnippetService snippetService;
 
     @GetMapping({"/dashboard", "dashboard/{folder}","/dashboard/{folder}/{id}"})
     public String getUserDashboard(@AuthenticationPrincipal CurrentUser currentUser,
-                                        @PathVariable(required = false) String folder,
-                                        @PathVariable(required = false) Long id, Model model) {
-        return snippetService.returnUserDashboard(currentUser, folder, id, model);
+                                   @PathVariable(required = false) String folder,
+                                   @PathVariable(required = false) Long id,
+                                   Model model) {
+        return snippetService.getUserDashboard(currentUser, folder, id, model);
     }
 
     @GetMapping("/add-snippet")
-    public String userAddSnippet(@AuthenticationPrincipal CurrentUser currentUser, Model model) {
+    public String userAddNewSnippetGet(@AuthenticationPrincipal CurrentUser currentUser,
+                                       Model model) {
         snippetService.getProgrammingLanguagesAndUserFoldersAndUserTags(currentUser, model);
-        model.addAttribute("snippet", new Snippet());
-        model.addAttribute("newSnippet", true);
-        return "user/user-snippet-manage";
+        return snippetService.userAddNewSnippetGet(model);
     }
 
     @PostMapping("/add-snippet")
-    public String userAddSnippetPost(@AuthenticationPrincipal CurrentUser currentUser,
+    public String userAddNewSnippetPost(@AuthenticationPrincipal CurrentUser currentUser,
                                      @ModelAttribute("snippet") @Valid Snippet snippet,
-                                     BindingResult result, HttpServletRequest request, Model model) {
+                                     BindingResult result,
+                                     HttpServletRequest request,
+                                     Model model) {
         if(result.hasErrors()) {
             snippetService.getProgrammingLanguagesAndUserFoldersAndUserTags(currentUser, model);
             return "user/user-snippet-manage";
         }
-
-        snippetService.setSnippetFolder(snippet, request.getParameter("inputtedFolder"));
-        snippetService.setSnippetTagsAndUserTags(snippet,request.getParameter("inputtedTags") , currentUser.getUser());
-        snippetService.saveSnippet(snippet, currentUser.getUser().getId());
-
-        return "redirect:/user/snippet-details/" + snippet.getId();
+        return snippetService.userAddNewSnippetPost(currentUser, snippet, request);
     }
 
     @GetMapping("/edit-snippet/{id}")
-    public String editUserSnippetGet(@AuthenticationPrincipal CurrentUser currentUser, @PathVariable Long id, Model model) {
-        Snippet snippet = snippetService.getUserSnippetByID(id);
-        if(snippet.getOwner().getId() == currentUser.getUser().getId()) {
-            model.addAttribute("snippet", snippet);
-            snippetService.getProgrammingLanguagesAndUserFoldersAndUserTags(currentUser, model);
-//            return "user/user-snippet-edit";
-            model.addAttribute("newSnippet", false);
-            return "user/user-snippet-manage";
-        } else {
-            return "public/error";
-        }
+    public String editUserSnippetGet(@AuthenticationPrincipal CurrentUser currentUser,
+                                     @PathVariable Long id,
+                                     Model model) {
+        return snippetService.editUserSnippetGet(currentUser, id, model);
     }
 
     @PostMapping("/edit-snippet/{id}")
-    public String editUserSnippetPost(@AuthenticationPrincipal CurrentUser currentUser, @Valid Snippet snippet, BindingResult result, HttpServletRequest request, Model model) {
+    public String editUserSnippetPost(@AuthenticationPrincipal CurrentUser currentUser,
+                                      @Valid Snippet snippet,
+                                      BindingResult result,
+                                      HttpServletRequest request,
+                                      Model model) {
         if(result.hasErrors()) {
             snippetService.getProgrammingLanguagesAndUserFoldersAndUserTags(currentUser, model);
             return "user/user-snippet-edit";
         }
-
-        snippetService.setSnippetFolder(snippet, request.getParameter("inputtedFolder"));
-        snippetService.setSnippetTagsAndUserTags(snippet,request.getParameter("inputtedTags") , currentUser.getUser());
-        snippetService.saveSnippet(snippet, currentUser.getUser().getId());
-
-        return "redirect:/user/snippet-details/" + snippet.getId();
+        return snippetService.editUserSnippetPost(currentUser, snippet, request, model);
     }
 
     @GetMapping("/snippet-details/{id}")
-    public String viewUserSnippetDetails(@AuthenticationPrincipal CurrentUser currentUser,@PathVariable Long id, Model model) {
-        Snippet snippet = snippetService.getUserSnippetByID(id);
-        if(snippet.getOwner().getId() == currentUser.getUser().getId()) {
-            model.addAttribute("snippet", snippet);
-            return "user/user-snippet-details";
-        } else {
-            return "public/error";
-        }
+    public String viewUserSnippetDetails(@AuthenticationPrincipal CurrentUser currentUser,
+                                         @PathVariable Long id,
+                                         Model model) {
+        return snippetService.viewUserSnippetDetails(currentUser, id, model);
     }
 
     @GetMapping("/delete-snippet/{id}")
